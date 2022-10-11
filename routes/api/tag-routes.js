0686b38ea4e5f76969f429880d2247207a086220
Vincent Toon <vincenttoon22@gmail.com>
2022-10-11 14:43:54 -0500

    tag routes and models tweaked for functionality

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product, ProductTag, Category } = require('../../models');
 
 // The `/api/tags` endpoint
 
@@ -16,9 +16,9 @@ router.get('/', (req, res) => {
         'product_name',
         'price',
         'stock',
-        'category_name'
+        'category_id'
       ]
-    }
+    },
   })
   .then(dbTagData => res.json(dbTagData))
   .catch(err => {
@@ -45,7 +45,7 @@ router.get('/:id', (req, res) => {
           'product_name',
           'price',
           'stock',
-          'category_name'
+          'category_id'
         ]
       }
     ]
@@ -100,7 +100,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
-  Tag.destroy(req.body, {
+  Tag.destroy({
     where: {
       id: req.params.id
     }
